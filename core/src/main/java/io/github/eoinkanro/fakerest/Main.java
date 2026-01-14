e0887fdec2f3172a1a6989536701ccfcf834b9f6
EoinKanro <54404008+EoinKanro@users.noreply.github.com>
2026-01-14 17:24:37 +0100

    Init

--- core/src/main/java/io/github/eoinkanro/fakerest/Main.java
+++ core/src/main/java/io/github/eoinkanro/fakerest/Main.java
@@ -1,4 +1,4 @@
-package io.github.eoinkanro;
+package io.github.eoinkanro.fakerest;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.

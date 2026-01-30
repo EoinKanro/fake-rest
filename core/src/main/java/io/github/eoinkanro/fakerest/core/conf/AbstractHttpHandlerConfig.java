a756a0a2ac4ef6caf3ca5c24810267dca8b6ffaf
EoinKanro <54404008+EoinKanro@users.noreply.github.com>
2026-01-30 21:38:34 +0100

    Add NonNull to config

--- core/src/main/java/io/github/eoinkanro/fakerest/core/conf/AbstractHttpHandlerConfig.java
+++ core/src/main/java/io/github/eoinkanro/fakerest/core/conf/AbstractHttpHandlerConfig.java
@@ -2,13 +2,16 @@ package io.github.eoinkanro.fakerest.core.conf;
 
 import io.github.eoinkanro.fakerest.core.model.HttpMethod;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
 public abstract class AbstractHttpHandlerConfig {
 
+    @NonNull
     private final String path;
+    @NonNull
     private final HttpMethod method;
 
     public abstract HttpHandlerType getType();
--- core/src/main/java/io/github/eoinkanro/fakerest/core/conf/impl/StaticHttpHandlerConfig.java
+++ core/src/main/java/io/github/eoinkanro/fakerest/core/conf/impl/StaticHttpHandlerConfig.java
@@ -3,12 +3,14 @@ package io.github.eoinkanro.fakerest.core.conf.impl;
 import io.github.eoinkanro.fakerest.core.conf.AbstractHttpHandlerConfig;
 import io.github.eoinkanro.fakerest.core.conf.HttpHandlerType;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
 public class StaticHttpHandlerConfig extends AbstractHttpHandlerConfig {
 
+    @NonNull
     private String responseBody;
     private int responseCode;
 
